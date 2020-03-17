79c5d42ccca24375021f5d78d2e78562aa24b464
kingeunji <kokopink12@gmail.com>
2020-03-17 10:02:58 +0900

    [Vue] API 설정 공통화 && 환경변수 설정

--- vue/vue-til/src/components/SignupForm.vue
+++ vue/vue-til/src/components/SignupForm.vue
@@ -13,7 +13,8 @@
             <input type="text" id="nickname" v-model="nickname"> 
         </div>
 
-        <button type="submit">login</button>
+        <button type="submit">회원가입</button>
+        <p> {{ logMessage }} </p>
     </form>
 </template>
 
@@ -25,18 +26,25 @@ export default {
         return {
             username : '',
             password : '',
-            nickname : ''
+            nickname : '',
+            logMessage : ''
         }
     },
     methods: {
-        submitForm() {
-            console.log("폼 제출");
+        async submitForm() {
             const userData = {
                 username : this.username,
                 password : this.password,
                 nickname : this.nickname
-            }
-            registerUser(userData);
+            };
+            const { data } = await registerUser(userData);
+            this.logMessage = `${data.username}님이 가입되었습니다.`;
+            this.initForm();
+        },
+        initForm() {
+            this.username = '';
+            this.password = '';
+            this.nickname = '';
         }
     }
 
