a5914006d3f24b319da21265f7f1a485b7c33d2e
kingeunji <kokopink12@gmail.com>
2020-04-18 23:10:07 +0900

    [NodeJS] jwt 회원가입/로그인 JWT 토큰 발급

---
+++ nodejs/jwt-example/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const config = require('./config');
+const port = process.env.PORT || 3000
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+
+app.set('jwt-secret', config.secret);
+
+app.get('/', (req, res) => {
+    res.send('Hello JWT')
+})
+
+// configure api router
+app.use('/api', require('./routes/api'))
+
+app.listen(port, () => {
+    console.log(`Express is running on port ${port}`)
+})
+
+mongoose.connect(config.mongodbUri)
+const db = mongoose.connection
+db.on('error', console.error)
+db.once('open', () => {
+    console.log('connected to mongodb server')
+})
---
+++ nodejs/jwt-example/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secret': 'SeCrEtKeYfOrHaShInG',
+    'mongodbUri': 'mongodb://kingeunji:bhj0525@ds217970.mlab.com:17970/jwt-example'
+}
