32819b6c6f1761bf3323eadbaab0f47d0df160fc
kingeunji <kokopink12@gmail.com>
2020-04-23 14:50:25 +0900

    [React] JSX 문법

--- react/projects/tutorial/src/App.js
+++ react/projects/tutorial/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello';
+
+const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24, // 기본 단위 px
+    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
+};
 
 function App() {
+  const name = 'react';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello/>
+      {name} 님! 환영합니다!
+      <div style={style}>영역</div>
+      <div className="gray-box"></div>
     </div>
   );
 }
