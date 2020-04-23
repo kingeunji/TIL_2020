268bc9c4f3fb208fa44b7762e196a03d85137576
kingeunji <kokopink12@gmail.com>
2020-04-23 14:56:04 +0900

    [React] Props 값을 통해 컴포넌트에게 전달

--- react/projects/tutorial/src/App.js
+++ react/projects/tutorial/src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import './App.css';
 import Hello from './components/Hello';
-
-const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24, // 기본 단위 px
-    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
-};
+import Wrapper from './components/Wrapper';
 
 function App() {
-  const name = 'react';
   return (
-    <div>
-      <Hello/>
-      {name} 님! 환영합니다!
-      <div style={style}>영역</div>
-      <div className="gray-box"></div>
-    </div>
+    <Wrapper>
+      <Hello name="react" color="red" />
+      <Hello color="pink" />
+    </Wrapper>
   );
 }
 
