5519687b3ba1f783e3f087b1c3f3b1569cfecb1a
kingeunji <kokopink12@gmail.com>
2020-04-23 16:07:48 +0900

    [React] map을 이용한 배열 렌더링

--- react/projects/tutorial/src/App.js
+++ react/projects/tutorial/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import InputSample from './components/InputSample';
+import UserList from './components/UserList';
 
 function App() {
   return (
     <>
-      <InputSample />
+      <UserList />
     </>
   );
 }
