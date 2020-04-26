ec572ccbd80929b361b72a3d5495fb42631d6267
eunji <kokopink12@gmail.com>
2020-04-26 23:21:23 +0900

    [React] AuthWrapper 컴포넌트 작성

--- react/projects/auth-example/src/pages/Auth.js
+++ react/projects/auth-example/src/pages/Auth.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as baseActions from '../redux/modules/base';
+import { AuthWrapper } from '../components/Auth';
 
 class Auth extends Component {
+    componentWillMount() {
+        this.props.BaseActions.setHeaderVisibility(false);
+    }
+
+    componentWillUnmount() {
+        this.props.BaseActions.setHeaderVisibility(true);
+    }
+
     render() {
         return (
-            <div>
-                Auth
-            </div>
+            <AuthWrapper>
+
+            </AuthWrapper>
         );
     }
 }
 
-export default Auth;
+export default connect(
+    (state) => ({
+
+    }),
+    (dispatch) => ({
+        BaseActions: bindActionCreators(baseActions, dispatch)
+    })
+)(Auth);
