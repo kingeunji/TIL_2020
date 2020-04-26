6150a2c408b49f035ac9fce74fcb20017f46ccf8
kingeunji <kokopink12@gmail.com>
2020-04-26 21:33:08 +0900

    [React] todolist 라우터 추가

--- react/projects/todolist/src/App.js
+++ react/projects/todolist/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom"
+
 import { createGlobalStyle } from 'styled-components'
 
 import { TodoProvider } from './TodoContext';
 import SignupForm from './components/SignupForm'
 import SignupTemplate from './components/SignupTemplate'
+
+import TodoTemplate from "./components/TodoTemplate";
+import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
+import TodoItem from "./components/TodoItem";
+import TodoCreate from "./components/TodoCreate";
 
 const GlobalStyle = createGlobalStyle `
     body {
@@ -14,12 +22,27 @@ const GlobalStyle = createGlobalStyle `
 
 function App() {
     return (
-        <TodoProvider>
-          <GlobalStyle />
-          <SignupTemplate>
-              <SignupForm/>
-          </SignupTemplate>
-        </TodoProvider>
+        <Router>
+            <GlobalStyle />
+            <Switch>
+                <Route path="/signup">
+                    <SignupTemplate>
+                        <SignupForm/>
+                    </SignupTemplate>
+                </Route>
+                <Route path="/todo">
+                    <TodoProvider>
+                      <TodoTemplate>
+                          <TodoHead/>
+                          <TodoList>
+                              <TodoItem/>
+                          </TodoList>
+                          <TodoCreate/>
+                      </TodoTemplate>
+                    </TodoProvider>
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
