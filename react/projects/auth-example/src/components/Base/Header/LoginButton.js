90d388817fefaa66359f3b7447ebc903ed9269cb
eunji <kokopink12@gmail.com>
2020-04-27 10:39:05 +0900

    [React] 로그인/회원가입 input에 redux 연동

--- react/projects/auth-example/src/components/Base/Header/LoginButton.js
+++ react/projects/auth-example/src/components/Base/Header/LoginButton.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from 'styled-components';
 import oc from 'open-color';
-import { Link } from 'react-router-dom';
 import { shadow } from '../../../lib/styleUtil';
 
 const BorderedButton = styled.div`
