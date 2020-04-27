2b534e6ecee9184fc5c02ee44845b0fe03547871
eunji <kokopink12@gmail.com>
2020-04-27 10:14:45 +0900

    [React] 회원가입폼 컴포넌트 작성

--- react/projects/auth-example/src/containers/Auth/Login.js
+++ react/projects/auth-example/src/containers/Auth/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AuthContent, InputWithLabel, AuthButton } from '../../components/Auth';
+import { AuthContent, InputWithLabel, AuthButton, RightAlignedLink } from '../../components/Auth';
 
 class Login extends Component {
     render() {
@@ -8,6 +8,7 @@ class Login extends Component {
                 <InputWithLabel label="이메일" name="email" placeholder="이메일"/>
                 <InputWithLabel label="비밀번호" name="password" placeholder="비밀번호" type="password"/>
                 <AuthButton>로그인</AuthButton>
+                <RightAlignedLink to="/auth/register">회원가입</RightAlignedLink>
             </AuthContent>
         );
     }
